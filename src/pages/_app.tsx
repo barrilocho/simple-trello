7ab607842d820b6d073baa7744ab5efd236f7f4d
Alejandro Barrios <barrilocho@gmail.com>
2022-05-02 07:33:07 -0500

    feat(app): add themes to app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
-const defaultTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+import { lightTheme, darkTheme } from '../themes';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
