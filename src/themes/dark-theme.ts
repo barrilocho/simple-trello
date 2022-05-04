f5ce38d9136193cade8c798c17a3b7a82afa7af6
Alejandro Barrios <barrilocho@gmail.com>
2022-05-03 21:33:46 -0500

    feat(colors): add colors to themes

--- src/themes/dark-theme.ts
+++ src/themes/dark-theme.ts
@@ -3,5 +3,17 @@ import { createTheme } from '@mui/material';
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
+    background: {
+      default: '#001B2E',
+    },
+    primary: {
+      main: '#ADB6C4',
+    },
+    secondary: {
+      main: '#294c60',
+    },
+    error: {
+      main: '#A26769',
+    },
   },
 });
--- src/themes/light-theme.ts
+++ src/themes/light-theme.ts
@@ -1,7 +1,20 @@
 import { createTheme } from '@mui/material';
+import { grey } from '@mui/material/colors';
 
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    background: {
+      default: '#ECEBF3',
+    },
+    primary: {
+      main: '#001B2E',
+    },
+    secondary: {
+      main: '#294c60',
+    },
+    error: {
+      main: '#A26769',
+    },
   },
 });
