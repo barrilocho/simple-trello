35fc9f8dc64315ac0b13ac4b51aee9b7c144108e
Alejandro Barrios <barrilocho@gmail.com>
2022-05-03 22:22:05 -0500

    feat(layout): add layout to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Layout } from '../components/layouts/Layout';
 
 const Home: NextPage = () => {
   return (
-    <Typography variant="h1" color="primary">
-      Simple trello
-    </Typography>
+    <Layout>
+      <Typography variant="h1" color="primary">
+        Simple trello
+      </Typography>
+    </Layout>
   );
 };
 
