158c2692f49f5430d06dad91915f0db8410b4e80
Alejandro Barrios <barrilocho@gmail.com>
2022-05-03 21:34:32 -0500

    feat(themes): add layout for application

---
+++ src/components/layouts/Layout.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout: FC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
+export default Layout;
