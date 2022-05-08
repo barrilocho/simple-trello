a36b361393cb85cda515fd20a9a2587ffd3a2c95
Alejandro Barrios <barrilocho@gmail.com>
2022-05-07 21:39:21 -0500

    feat(entries): add entries context

---
+++ src/context/entries/EntriesProvider.tsx
@@ -0,0 +1,23 @@
+import { FC, useReducer } from 'react';
+import { EntriesContext, entriesReducer } from './';
+
+export interface EntriesState {
+  entries: [];
+}
+
+const Entries_INITIAL_STATE: EntriesState = {
+  entries: [],
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const EntriesProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
+  return (
+    <EntriesContext.Provider value={{ ...state }}>
+      {children}
+    </EntriesContext.Provider>
+  );
+};
