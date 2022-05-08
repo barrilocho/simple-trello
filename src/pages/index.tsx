ea956d91d03bd104eff50ef567a81ea18b05b10e
Alejandro Barrios <barrilocho@gmail.com>
2022-05-07 21:30:41 -0500

    feat(grid): principal layout grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
-import { Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
 import type { NextPage } from 'next';
 import { Layout } from '../components/layouts/Layout';
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        Simple trello
-      </Typography>
+    <Layout title="Home - Simple trello">
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="To Do"></CardHeader>
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Doing"></CardHeader>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Done"></CardHeader>
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
